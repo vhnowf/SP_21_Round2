0e78c662a227e8a00af7507646e8a3d56d8c7cf4
vhnowf <vohoangnamntm@gmail.com>
2021-12-19 09:03:45 +0700

    Initialize basecode

---
+++ app/Http/Controllers/Admin/HomeController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+class HomeController
+{
+    public function index()
+    {
+        $settings1 = [
+            'chart_title'           => 'Revenue by day',
+            'chart_type'            => 'bar',
+            'report_type'           => 'group_by_date',
+            'model'                 => 'App\Models\Purchase',
+            'group_by_field'        => 'created_at',
+            'group_by_period'       => 'day',
+            'aggregate_function'    => 'sum',
+            'aggregate_field'       => 'price',
+            'filter_field'          => 'created_at',
+            'filter_days'           => '30',
+            'group_by_field_format' => 'Y-m-d H:i:s',
+            'column_class'          => 'col-md-12',
+            'entries_number'        => '5',
+        ];
+        return view('admin.home');
+    }
+}
---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.admin')
+@section('content')
+<div class="content">
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="card">
+                <div class="card-header">
+                    Dashboard
+                </div>
+
+                <div class="card-body">
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
