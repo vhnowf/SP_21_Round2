c45b1dba10f2893a4ea06877323c308c7eaca9ac
vhnowf <vohoangnamntm@gmail.com>
2021-12-27 14:52:28 +0700

    Fix route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\InboxController;
+use App\Http\Controllers\FeedbackController;
 
 /*
 |--------------------------------------------------------------------------
